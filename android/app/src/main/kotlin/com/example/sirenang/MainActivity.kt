3825f0f9b2e1e76eb5b35446666d3de32004ad0e
ijlnflhbrzdev <ijlnflhbrzdev@gmail.com>
2023-01-19 14:35:31 +0700

    INITIAL APP SIRENANG

---
+++ android/app/src/main/kotlin/com/example/sirenang/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sirenang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'HomeView is working',
+          // style: GoogleFonts.poppins().copyWith(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
