9269dd4c16877de4dc868b2665413697e5d0ac57
ijlnflhbrzdev <ijlnflhbrzdev@gmail.com>
2023-01-19 15:23:19 +0700

    SLICING UI BOTTOM NAVIGATION BAR

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,17 +8,51 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          // style: GoogleFonts.poppins().copyWith(fontSize: 20),
+    return GetBuilder<HomeController>(
+      builder: (controller) => Scaffold(
+        appBar: AppBar(
+          title: const Text('HomeView'),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+            child: IndexedStack(
+          index: controller.tabIndexBottomNavigationBar,
+          children: const [
+            Center(child: Text('EXPLORE 1')),
+            Center(child: Text('SAVED 2')),
+            Center(child: Text('SWIM 3')),
+            Center(child: Text('INBOX 4')),
+            Center(child: Text('PROFILE 5')),
+          ],
+        )),
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.red,
+          onTap: controller.changeTabIndex,
+          currentIndex: controller.tabIndexBottomNavigationBar,
+          // showSelectedLabels: false,
+          // showUnselectedLabels: false,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            _bottomNavigationBarItems(
+                iconBar: Icons.search, labelName: 'Explore'),
+            _bottomNavigationBarItems(
+                iconBar: Icons.favorite, labelName: 'Saved'),
+            _bottomNavigationBarItems(iconBar: Icons.cable, labelName: 'Swim'),
+            _bottomNavigationBarItems(
+                iconBar: Icons.person, labelName: 'Inbox'),
+            _bottomNavigationBarItems(
+                iconBar: Icons.home, labelName: 'Profile'),
+          ],
         ),
       ),
     );
   }
+}
+
+_bottomNavigationBarItems({IconData? iconBar, String? labelName}) {
+  return BottomNavigationBarItem(
+    icon: Icon(iconBar),
+    label: labelName,
+  );
 }
